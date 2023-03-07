df9d7d5d03eca37039abd82c3e091b3f852efe26
enebin <enebin777@gmail.com>
2023-03-07 20:08:12 +0900

    Compression & Hugging

--- UIKitPlayground/ViewController.swift
+++ UIKitPlayground/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    private let leftLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Lorem ipsum dolor sit amet, consectetur"
+        label.textColor = .white
+        label.backgroundColor = .blue
+        return label
+    }()
+    
+    private let rightLabel: UILabel = {
+        let label = UILabel()
+        label.text = "adipiscing elit. Curabitur commodo aliquam."
+        label.textColor = .white
+        label.backgroundColor = .orange
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.addSubview(self.leftLabel)
+        self.view.addSubview(self.rightLabel)
+        
+        self.leftLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
+        self.leftLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(56)
+            $0.left.equalToSuperview()
+        }
+        
+        self.rightLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        self.rightLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(56)
+            $0.left.equalTo(self.leftLabel.snp.right)
+            $0.right.equalToSuperview()
+        }
     }
-
-
 }
-

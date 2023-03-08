8689f8eb761ea052c3f92c716f9e1cc95028117f
enebin <enebin777@gmail.com>
2023-03-08 18:17:14 +0900

    [week-1] UIView - zoom in bound test

--- UIKitPlayground/ViewController.swift
+++ UIKitPlayground/ViewController.swift
@@ -10,20 +10,26 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        scrollView.maximumZoomScale = 4.0
         scrollView.delegate = self
     }
     
     override func viewDidAppear(_ animated: Bool){
-        let vc = AnimateViewController()
-        self.show(vc, sender: self)
+//        let vc = AnimateViewController()
+//        self.show(vc, sender: self)
     }
 }
 
 extension ViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.imageView
+    }
+    
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         print("frame: ", scrollView.frame)
         print("bounds: ", scrollView.bounds)
